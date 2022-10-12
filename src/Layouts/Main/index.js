21db066c751f38517165b772126407f8376c0e5c
prakash19csu214 <prakashsingh112000@gmail.com>
2022-10-12 18:38:28 +0530

    Main Header Footer Layout

---
+++ src/Layouts/Main/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Home from '../../Pages/Home';
+import About from '../../Pages/About Us';
+import Header from '../Header';
+import Footer from '../Footer';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+
+export default class Main extends Component {
+  render() {
+    return (
+        <div>   
+          <Header />
+          <Switch>
+            <Route path= "/home" component={Home} />
+            {/* exact used to prevent switch from confusion with other menu links */}
+            <Route exact path="/aboutus" component={About} />
+            <Redirect to="/home" />
+          </Switch>
+          <Footer />
+        </div>
+      );
+  }
+}
